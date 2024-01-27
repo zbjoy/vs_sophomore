0539c298aa68dad324447ed6c230a3d4521412d0
zbjoy <664266432@qq.com>
2024-01-27 12:12:38 +0800

    测试

---
+++ qt_sophomore_gitpush/qt_sophomore_gitpush/main.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+
+
+void git_Push(char* cmd)
+{
+	system("E:");
+	system("cd E:\\code\\qt_sophomore");
+	system("git add ./");
+	char cmdStr[1024] = { 0 };
+	sprintf(cmdStr, "%s%s", "git commit -m", cmd);
+	system(cmdStr);
+	system("git push");
+}
+
+int main()
+{
+	char temp[1024] = { 0 };
+	std::cin >> temp;
+	git_Push(temp);
+	return 0;
+}
